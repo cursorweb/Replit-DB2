8018eb1888ecf7e8a7f5b0fdf5f555bb9be7766f
Junhao Zhang <junhaodev@gmail.com>
2021-06-08 18:18:08 -0500

    Added more tests for methods

--- test/db.test.js
+++ test/db.test.js
@@ -22,7 +22,7 @@ describe("get method", () => {
         try {
             await db.get("key");
             expect(false).toEqual(true); // it was supposed to throw error!
-        } catch (e) {
+        } catch {
             expect(true).toEqual(true); // success!
         }
     });
@@ -35,5 +35,52 @@ describe("set method", () => {
 
     test("set db value", async () => {
         expect(await db.set("key", "value")).toBeUndefined();
+    });
+
+    test("set invalid value", async () => {
+        try {
+            await db.set("key", new BigInt(69));
+            expect(false).toEqual(true); // Supposed to throw error!
+        } catch {
+            expect(true).toEqual(true);
+        }
+    });
+});
+
+describe("delete method", () => {
+    test("delete key", async () => {
+        await db.set("key", "value");
+        await db.delete("key");
+
+        expect(await db.get("key")).toEqual(null);
+    });
+});
+
+describe("list method", () => {
+    test("list keys", async () => {
+        await db.set("key", "value");
+        await db.set("key2", "value2");
+        await db.set("key3", "value3");
+
+        expect(await db.list()).toEqual(["key", "key2", "key3"]);
+    });
+
+    test("list keys prefix", async () => {
+        await db.set("key", "value");
+        await db.set("akey2", "value2");
+        await db.set("key3", "value3");
+
+        expect(await db.list("k")).toEqual(["key", "key3"]);
+    });
+});
+
+describe("empty method", () => {
+    test("empty db", () => {
+        await db.set("key", "value");
+        await db.set("key2", "value2");
+        await db.set("key3", "value3");
+        await db.empty();
+
+        expect(await db.list()).toEqual([]);
     });
 });
